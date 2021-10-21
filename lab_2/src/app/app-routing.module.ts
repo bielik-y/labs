989ddb75104d2bacfefb417d428424d8c84e1b41
bielik-y <71470363+bielik-y@users.noreply.github.com>
2021-10-22 00:52:11 +0300

    Update app-routing.module.ts

--- lab_2/src/app/app-routing.module.ts
+++ lab_2/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
+//вова лох
 const routes: Routes = [
   {path:'', component: LoginComponent},
   {path:'home', canActivate: [AuthGuard], component: HomeComponent}
